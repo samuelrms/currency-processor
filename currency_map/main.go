01d56b217583aa94d9e46b42ceab67b50f49662f
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2025-05-16 01:58:59 -0300

    Adds currency map
    Adds a currency map that associates currency codes and common names to standard currency codes.
    
    This map allows for flexible currency input and ensures that different representations of the same currency are correctly identified.

---
+++ currency_map/main.go
@@ -0,0 +1,62 @@
+package currency_map
+
+var CurrencyMap = map[string]string{
+	// Brazilian Real
+	"BRL":  "BRL",
+	"REAL": "BRL",
+
+	// US Dollar
+	"USD":                      "USD",
+	"DOLAR":                    "USD",
+	"DÓLAR":                    "USD",
+	"DOLAR DOS ESTADOS UNIDOS": "USD",
+	"DÓLAR DOS ESTADOS UNIDOS": "USD",
+	"DOLLAR":                   "USD",
+
+	// Australian Dollar
+	"AUD":               "AUD",
+	"DOLAR AUSTRALIANO": "AUD",
+	"DÓLAR AUSTRALIANO": "AUD",
+	"AUSTRALIAN DOLLAR": "AUD",
+
+	// Canadian Dollar
+	"CAD":             "CAD",
+	"DOLAR CANADENSE": "CAD",
+	"CANADIAN DOLLAR": "CAD",
+
+	// Swiss Franc
+	"CHF":          "CHF",
+	"FRANCO SUICO": "CHF",
+	"FRANCO SUÍÇO": "CHF",
+	"SWISS FRANC":  "CHF",
+
+	// Danish Krone
+	"DKK":                "DKK",
+	"COROA DINAMARQUESA": "DKK",
+	"DANISH KRONE":       "DKK",
+
+	// Euro
+	"EUR":  "EUR",
+	"EURO": "EUR",
+
+	// British Pound
+	"GBP":             "GBP",
+	"LIBRA ESTERLINA": "GBP",
+	"BRITISH POUND":   "GBP",
+	"POUND":           "GBP",
+
+	// Japanese Yen
+	"JPY":  "JPY",
+	"IENE": "JPY",
+	"YEN":  "JPY",
+
+	// Norwegian Krone
+	"NOK":              "NOK",
+	"COROA NORUEGUESA": "NOK",
+	"NORWEGIAN KRONE":  "NOK",
+
+	// Swedish Krona
+	"SEK":           "SEK",
+	"COROA SUECA":   "SEK",
+	"SWEDISH KRONA": "SEK",
+}
